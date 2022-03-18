8fedc0b43d454ea6faaa126d38d2f0cd4695073e
123木头人 <1206993746@qq.com>
2022-03-18 18:40:12 +0800

    🔧 build(连接模块): 删除

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ArticleModule } from './modules/article/article.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
 import { MenuModule } from './modules/menu/menu.module';
-import { ConnectModule } from './modules/connect/connect.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ArticleModule, UserModule, MenuModule, ConnectModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ArticleModule,
+    UserModule,
+    MenuModule,
+  ],
 })
 export class AppModule {}
